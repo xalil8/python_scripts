bd74c33618f368af02da8ed51f07d426765fc1fb
halil özcan <46287166+xalil8@users.noreply.github.com>
2022-10-12 15:24:41 +0300

    for. make 222 class labels  into one class

---
+++ yolov5_label_customize.py
@@ -0,0 +1,20 @@
+import os 
+import pandas as pd 
+
+with os.scandir("/Users/halil/Downloads/datas/labels/test") as files:  # from this dir , labels getting
+    counter = -1    #for naming txt files going  0 to n 
+    for file in files:
+        counter += 1
+        xalil_name = "label" + str(counter)
+        #if file.name.endswith("migros_005.txt" ):  #this line used for testing the scrip, can be removed 
+        t = open(file,"r+")
+        new_file = open(f"/Users/halil/Desktop/labels/{xalil_name}.txt", "w+")  #new labels writing on this dir 
+
+        for line in t :                
+            a = line.split(' ')   #line by line splitting each line elements by space 
+            a[0] = "0"             #whatever label is replacing with 0 
+            x = ""
+            for i in a:
+                x += i+" "
+            new_file.write(x + '\n')  #writing lines 
+        new_file.close()
