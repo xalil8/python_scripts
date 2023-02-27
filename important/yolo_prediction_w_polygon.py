02f433ed734aa0562cfb9ed5d7d13661fad12d34
Halil Ozcan <46287166+xalil8@users.noreply.github.com>
2023-02-28 00:08:21 +0300

    Update yolo_prediction_w_polygon.py

--- important/yolo_prediction_w_polygon.py
+++ important/yolo_prediction_w_polygon.py
@@ -1,4 +1,3 @@
-#area = [[467, 422], [533, 407], [150, 199], [118, 206]]
 
 #TODO make polygons filled with transparant color
 
@@ -6,21 +5,26 @@ import torch
 import cv2
 import numpy as np
 import ssl
-#points = np.array([[112, 278], [463, 573], [575, 537], [1136, 891], [1185, 821], [601, 489], [504, 510], [149, 269]])
-#points = np.array([[110, 275], [435, 566], [558, 537], [154, 271]])
-points = np.array([[86, 258], [422, 595], [563, 565], [1139, 926], [1215, 802], [606, 459], [507, 481], [153, 257]])
-#points = points.reshape((-1,1,2))
 ssl._create_default_https_context = ssl._create_unverified_context
 
-source_video_path = "custom_plot/deneme2.mp4"
 
-video_cap=cv2.VideoCapture('polyline/polygon.mp4')
+source_video_path = "polyline/polygon.mp4"
+video_saving_path = source_video_path[:len(source_video_path)-4:]+"_output.mp4"
 
-#model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)
-
+polygon_points = np.array( [[446, 599], [575, 559], [1217, 954], [1273, 832], [592, 465], [507, 485], [130, 237], [66, 248]])
 model = torch.hub.load("ultralytics/yolov5","custom",path="polyline/best.pt",force_reload=True)
 
 
+#to use with default yolo model
+#model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)
+
+video_cap=cv2.VideoCapture(source_video_path)
+
+fps = video_cap.get(cv2.CAP_PROP_FPS)
+width, height = int(video_cap.get(cv2.CAP_PROP_FRAME_WIDTH)),int(video_cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+desired_fps = 20
+result = cv2.VideoWriter(video_saving_path, cv2.VideoWriter_fourcc(*'mp4v') ,desired_fps, (width,height))
 
 
 count=0
@@ -29,13 +33,13 @@ while video_cap.isOpened():
 
     if not ret:
         break
+    #ADJUST FPS
     count += 1
-    if count % 3 != 0:
+    if count % 1 != 0:
         continue
 
-    #roi = frame[200:400,200:400]
     results = model(frame)
-    cv2.polylines(frame, np.int32([points]), True, (255,0,0),3)
+    cv2.polylines(frame, np.int32([polygon_points]), True, (255,0,0),3)
 
     for index, row in results.pandas().xyxy[0].iterrows():
         class_name, x1, y1, x2, y2 = row['name'],int(row['xmin']), int(row['ymin']), int(row['xmax']), int(row['ymax'])
@@ -43,23 +47,25 @@ while video_cap.isOpened():
         if class_name =="insan":
             center_x, center_y = int((x1+x2)/2), y2
 
-            area_check = cv2.pointPolygonTest(np.int32([points]),((center_x,center_y)), False)
+            area_check = cv2.pointPolygonTest(np.int32([polygon_points]),((center_x,center_y)), False)
 
+            #if person are in polygon (safe) area 
             if area_check ==1:
                 #cv2.circle(frame,(center_x,center_y),3,(255,0,0),-1)
                 cv2.rectangle(frame,(x1,y1),(x2,y2),(0,255,0),2)
                 cv2.putText(frame, class_name, (x1,y1-10), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0,255,0), 2)
 
-
+            #person are in danger zone 
             else:
                 cv2.rectangle(frame,(x1,y1),(x2,y2),(0,0,255),2)
                 cv2.putText(frame, class_name, (x1,y1-10), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0,0,255), 2)
+                cv2.putText(frame, "TEHLIKELI BOLGEDE INSAN VAR", (10,40), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0,0,255), 2)
 
 
 
-    #cv2.polylines(frame,np.array(area,np.int32),True, (0,0,255),2)
     cv2.imshow("ROI",frame)
-
+    print(f"frame {count} writing")
+    result.write(frame)
 
 
     if cv2.waitKey(1) == ord('q'):
@@ -67,4 +73,6 @@ while video_cap.isOpened():
 
 
 video_cap.release()
+result.release()
 cv2.destroyAllWindows()
+print("process done")
