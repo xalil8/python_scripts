4fffe8a43921d7650d8c4603dc80ea22cc5c7825
Halil Ozcan <46287166+xalil8@users.noreply.github.com>
2023-05-03 15:06:05 +0300

    upadet for dynamically and without library needed

--- diattack/logo_adder/xalil_logo_adder.py
+++ diattack/logo_adder/xalil_logo_adder.py
@@ -1,22 +1,45 @@
 import cv2
-import cvzone
-
+import numpy as np 
 #setup
-source_video_path = "logo_adder/new_polygon_polygon.mp4"
-video_saving_path = source_video_path[:len(source_video_path)-4:]+"_final_logo.mp4"
+source_video_path = "demos/demo2.mp4"
+video_saving_path = source_video_path[:len(source_video_path)-4:]+"_wlogo.mp4"
 logo_path = "logo_adder/divisor.png"
 
 
+def overlayPNG(imgBack, imgFront, pos=[0, 0]):
+    hf, wf, cf = imgFront.shape
+    hb, wb, cb = imgBack.shape
+    *_, mask = cv2.split(imgFront)
+    maskBGRA = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGRA)
+    maskBGR = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)
+    imgRGBA = cv2.bitwise_and(imgFront, maskBGRA)
+    imgRGB = cv2.cvtColor(imgRGBA, cv2.COLOR_BGRA2BGR)
+
+    imgMaskFull = np.zeros((hb, wb, cb), np.uint8)
+    imgMaskFull[pos[1]:hf + pos[1], pos[0]:wf + pos[0], :] = imgRGB
+    imgMaskFull2 = np.ones((hb, wb, cb), np.uint8) * 255
+    maskBGRInv = cv2.bitwise_not(maskBGR)
+    imgMaskFull2[pos[1]:hf + pos[1], pos[0]:wf + pos[0], :] = maskBGRInv
+
+    imgBack = cv2.bitwise_and(imgBack, imgMaskFull2)
+    imgBack = cv2.bitwise_or(imgBack, imgMaskFull)
+
+    return imgBack
 
 fourcc = cv2.VideoWriter_fourcc(*'mp4v')
 video_cap = cv2.VideoCapture(source_video_path)
 
 width, height = int(video_cap.get(cv2.CAP_PROP_FRAME_WIDTH)),int(video_cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 writer = cv2.VideoWriter(video_saving_path, cv2.VideoWriter_fourcc(*'mp4v') ,video_cap.get(cv2.CAP_PROP_FPS), (width,height))
-
+print(width,height,"##########################")
 
 logo = cv2.imread(logo_path, cv2.IMREAD_UNCHANGED)
-logo = cv2.resize(logo, (200, 48)) #200, 48
+
+k = 1 if width<1500 else 2 
+
+logo_x = 200 *k
+logo_y = 48 *k
+logo = cv2.resize(logo, (logo_x,logo_y)) #200, 48
 
 counter =0
 while video_cap.isOpened():
@@ -26,7 +49,7 @@ while video_cap.isOpened():
 
     if ret:
 
-        frame = cvzone.overlayPNG(frame, logo, [3700, 1300])  #1050, 650
+        frame = overlayPNG(frame, logo, [width-logo_x-50, height-logo_y-50])  #1050, 650
 
         
         counter +=1
